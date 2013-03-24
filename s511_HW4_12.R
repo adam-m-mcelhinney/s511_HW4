1a5c9917f15a0272adaa17b76a439cd107104da0
Adam McElhinney <adam.m.mcelhinney@gmail.com>
2013-03-24 10:16:23 -0500

    Foo

---
+++ s511_HW4_12.R
@@ -0,0 +1,8 @@
+### Stat 511 HW 4 #12
+# Suppose X~Unif(0;1). Use Monte Carlo integration to approximate the
+# moment-generating function M_X(u) of X on the interval u in (-2,2). Draw
+# a plot of your approximation with the true mgf overlaid and remark on the
+# quality of the approximation. 
+# [Hints: 
+# (i) T = 1000 Monte Carlo samples should be enough; 
+# (ii) you can use the same Monte Carlo samples for each point u on the discrete grid.]
