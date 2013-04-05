a5d08851f2626ee8082dc6b7e51fa0eaec2db983
Adam McElhinney <adam.m.mcelhinney@gmail.com>
2013-04-05 11:38:00 -0500

    Changed, should be working now

--- s511_HW4_12.R
+++ s511_HW4_12.R
@@ -1,8 +1,42 @@
-### Stat 511 HW 4 #12
-# Suppose X~Unif(0;1). Use Monte Carlo integration to approximate the
-# moment-generating function M_X(u) of X on the interval u in (-2,2). Draw
-# a plot of your approximation with the true mgf overlaid and remark on the
-# quality of the approximation. 
-# [Hints: 
-# (i) T = 1000 Monte Carlo samples should be enough; 
-# (ii) you can use the same Monte Carlo samples for each point u on the discrete grid.]
+# Draw T uniform samples from U(0,1)
+
+T<-1000
+#T<-5
+a<-0
+b<-1
+
+data<-runif(T,a, b)
+
+# Generate uniform list between (-2,2)
+#u<-seq(-2,2,.01)
+u<-seq(-2,2,.1)
+
+
+mgf_mc<-function(data, u_i){
+n=length(data)
+return ((1/n)*sum(exp(data*u_i)))
+                }
+
+# Actual MGF
+# Taken from wikipedia
+mgf<-function(u_i,a,b){
+    return (
+        (exp(u_i*b)-exp(u_i*a))/(u_i*(b-a))
+        )
+        }
+mgf(u_i=1,a,b)
+
+
+
+actual_mgf<-sapply(u,mgf,a=a,b=b)
+
+out<-vector()
+for (i in 1:length(u)){
+    u_i=u[i]
+    out<-c(out, mgf_mc(data,u_i))
+            }
+
+plot(u,out, type='l', col='blue')
+points(u,actual_mgf, col='red')
+
+
